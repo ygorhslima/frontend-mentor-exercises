671af1ed0e8085e79978c546c307534f2695eae0
ygorhslima <ygorlima.dev06@gmail.com>
2025-06-23 14:09:18 -0300

    começando o projeto

---
+++ conference-ticket-generator-main/script.js
@@ -0,0 +1,5 @@
+let full_name = document.getElementById("full_name").value
+let email_address = document.getElementById("email_address").value 
+let github_username = document.getElementById("github_username").value
+
+console.log(full_name)
