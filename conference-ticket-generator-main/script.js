ad4ea3506495b63fdf7c1e560c0d08a4c895cfa9
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-06-25 15:52:08 -0300

    fazendo modificações

---
+++ conference-ticket-generator-main/script.js
@@ -0,0 +1,14 @@
+const form__upload_icon = document.querySelector("#form__upload-icon")
+const upload_arquivo = document.querySelector("#upload-arquivo")
+
+const full_name = document.querySelector("#full_name")
+const email_address = document.querySelector("#email_address")
+
+const github_username = document.querySelector("#github_username")
+
+const btn_gerar_ticket = document.querySelector("#btn_gerar_ticket")
+
+
+btn_gerar_ticket.addEventListener("click",()=>{
+    
+})
