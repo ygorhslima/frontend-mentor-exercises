5353cc59dda87820196f0deeee00097df6e66bc8
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-08-03 21:05:59 -0300

    consertando o menu para mobile

--- space-tourism-website-figma/src/scripts/navigation.js
+++ space-tourism-website-figma/src/scripts/navigation.js
@@ -4,8 +4,10 @@ const menuLateral = document.getElementById('menuLateral');
 
 abrirMenu.addEventListener('click', () => {
   menuLateral.classList.add('mostrar');
+  abrirMenu.style.display = 'none'
 });
 
 fecharMenu.addEventListener('click', () => {
   menuLateral.classList.remove('mostrar');
+  abrirMenu.style.display = 'block'
 });
