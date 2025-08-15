9eb95068b951767bd6788b110948208e40b3d046
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-08-15 16:20:24 -0300

    .

--- space-tourism-website-figma/src/scripts/crew-navigation.js
+++ space-tourism-website-figma/src/scripts/crew-navigation.js
@@ -2,13 +2,19 @@ const items = document.querySelectorAll(".list .item");
 const nextBtn = document.querySelector(".btn_next");
 const prevBtn = document.querySelector(".btn_prev");
 const indicators = document.querySelectorAll(".indicator-crew");
-
 let currentIndex = 0;
 
 function showItem(index) {
     items.forEach((item, i) => {
         item.classList.toggle("active", i === index);
-        indicators[i].classList.toggle("active", i === index);
+        
+        if (i === index) {
+            indicators[i].classList.add("active");
+            indicators[i].style.backgroundColor = "black"; // indicador ativo
+        } else {
+            indicators[i].classList.remove("active");
+            indicators[i].style.backgroundColor = "transparent"; // desativa os outros
+        }
     });
 }
 
