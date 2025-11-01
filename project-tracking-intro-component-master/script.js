d05a43e245eda0c3d79e8ad7920fdb7b6f35399d
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-11-01 10:59:50 -0300

    ajustando hierarquia de elementos HTML com z-index e criação do menu

--- project-tracking-intro-component-master/script.js
+++ project-tracking-intro-component-master/script.js
@@ -1,5 +1,27 @@
-const body = document.body
+const body = document.body;
 
-const element = document.createElement("div")
-element.setAttribute("class","element")
-body.appendChild(element)
+// Cria o elemento de fundo decorativo
+const element = document.createElement("div");
+element.setAttribute("class", "element");
+body.appendChild(element);
+
+// Seleciona os elementos do menu
+const btnMenuClicked = document.querySelector(".btnMenuClicked");
+const nav = document.querySelector("header nav");
+const menuIcon = btnMenuClicked.querySelector("img");
+
+// Variável de controle
+let menuOpen = false;
+
+// Evento de clique no botão
+btnMenuClicked.addEventListener("click", () => {
+  menuOpen = !menuOpen;
+
+  if (menuOpen) {
+    nav.classList.add("menu-active");
+    menuIcon.src = "images/icon-close.svg"; // muda ícone para "X"
+  } else {
+    nav.classList.remove("menu-active");
+    menuIcon.src = "images/icon-hamburger.svg"; // volta para ícone hambúrguer
+  }
+});
